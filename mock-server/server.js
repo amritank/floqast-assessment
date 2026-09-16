8945c47359d954c6bf15c5e1e7252011a253476d
amritank <amrita.sasi.nair@gmail.com>
2026-09-15 23:35:43 -0700

    added server logging

--- mock-server/server.js
+++ mock-server/server.js
@@ -1,8 +1,10 @@
 import express from "express";
 import userRoutes from "./routes/users.js";
+import { createServerLogger } from "./middleware/logger.js";
 
 const PORT = process.env.PORT ?? 3001;
 const app = express();
+app.use(createServerLogger());
 app.use(express.json());
 app.use(express.static("mock-ui"));
 app.use("/api/users", userRoutes);
